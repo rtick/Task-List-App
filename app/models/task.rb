37744b73325ee2b7316bd2c7eb8996cec0ad65a3
Ryan Tick <rtick@nd.edu>
2015-11-10 14:47:02 -0500

    Added in validations and tested them to ensure accuracy.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
 	belongs_to :task_list
+	validates :title, presence: true
+	validates :priority, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10}
+	validates :due_date, presence: true
+	validates :task_list_id, presence: true
 end
