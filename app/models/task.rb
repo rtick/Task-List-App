50325e0d2102f7e8dc774dfaba5a3b6b0684c70f
Jonathan Arp <jarp@nd.edu>
2015-11-29 20:31:42 -0500

    notes

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ActiveRecord::Base
 	belongs_to :task_list
 	validates :title, presence: true
 	validates :priority, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10}
+	# jarp: for a range, you should use 'inclusion: { in: 1..10 }'. one rule and more expressive
 	validates :due_date, presence: true
 	validates :task_list_id, presence: true
 end
