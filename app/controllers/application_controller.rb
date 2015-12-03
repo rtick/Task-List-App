fad459f0092220c5b8bbfc82f421fcd833dc6fd0
Ryan Tick <rtick@nd.edu>
2015-12-02 21:37:36 -0500

    Finishing Touches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout 'todo_list'
+  before_action :get_all_task_lists
+  def get_all_task_lists
+	@all_task_lists = TaskList.all
+  end
 end
